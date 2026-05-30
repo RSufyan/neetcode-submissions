85f573752735e03e05827f629a5330c0b70b5089
Rothwan Sufyan <79485199+RSufyan@users.noreply.github.com>
2026-05-30 13:09:07 -0400

    Add: is-palindrome - submission-22

---
+++ Data Structures & Algorithms/is-palindrome/submission-22.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        ss = ''
+        for c in s:
+            if c.isalnum():
+                ss += c.lower()
+        return ss == ss[::-1]
+        
